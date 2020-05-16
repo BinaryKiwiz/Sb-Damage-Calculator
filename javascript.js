c1683507692a2e1ca35c8f83b6adaef7246472dd
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-05-16 16:39:38 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,6 +7,7 @@ var FinalDamage = 0;
 var CombatLevel = 0;
 var SharpDamage = 0;
 var CombatDamage = 0;
+var FirstStrikeLevel = 0;
 
 function Calculate(){
   WeaponDamage = parseInt(document.getElementById("WeaponDamage").value, 10);
@@ -14,14 +15,19 @@ function Calculate(){
   SharpLevel = parseInt(document.getElementById("SharpLevel").value, 10);
   CritDamage = parseInt(document.getElementById("CritDamage").value, 10);
   CombatLevel = parseInt(document.getElementById("CombatLevel").value, 10);
+  FirstStrikeLevel = parseInt(document.getElementById("FirstStrikeLevel").value, 10);
   
   BaseDamage = (5 + WeaponDamage + (Strength / 5)) * (1 + Strength/100);
+  FirstStrikeDamage = BaseDamage * (FirstStrikeLevel * 0.25);
   SharpDamage = BaseDamage * (SharpLevel * 0.05);
   CombatDamage = BaseDamage * (CombatLevel * 0.04);
   BaseDamage += SharpDamage + CombatDamage;
   
   FinalDamage = BaseDamage * (1 + CritDamage/100);
   
+  FirstStrikeDamage = (BaseDamage + FirstStrikeDamage) * (1 + CritDamage/100);
+  
   document.getElementById("BaseDamage").innerHTML = BaseDamage;
   document.getElementById("FinalDamage").innerHTML = FinalDamage;
+  document.getElementById("FirstStrikeDamage").innerHTML = FirstStrikeDamage;
 }
